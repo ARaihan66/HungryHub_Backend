80e3ccebcd8f14a861ab56a7a2c435da85f9c6b1
Abu Raihan <aburaihancse066@gmail.com>
2024-05-09 21:40:29 +0600

    user model added

---
+++ Models/UserModel.js
@@ -0,0 +1,32 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+
+const userSchema = Schema({
+  userName: {
+    type: String,
+    required: [true, "Please provide your name."],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide your password"],
+  },
+  cartItem: {
+    type: Array,
+    default: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "food",
+    },
+  },
+  otp: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const userModel = model("user", userSchema);
+
+module.exports = { userModel };
